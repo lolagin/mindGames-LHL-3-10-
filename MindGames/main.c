8796a94139c5d428daae49da9d8608c0f72f1530
Jeffrey Rosenthal <jeffrey.rosenthal@gmail.com>
2015-03-10 16:51:00 -0700

    Initial Commit

---
+++ MindGames/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MindGames
+//
+//  Created by Jeffrey C Rosenthal on 3/10/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
